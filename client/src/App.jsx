4f47fa2d6ee1257895239bd6ba77eaca81c927d4
HirthikRaj <a.hirthikraj@gmail.com>
2025-12-24 10:03:10 +0530

    Created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
-import './index.css'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Projects from './pages/Projects.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Signin from './pages/Signin.jsx'
+import Signup from './pages/Signup.jsx'
 export default function App() {
   return (
     <h1 className='text-3xl text-red-500'>App</h1>
